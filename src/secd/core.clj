47fd9fc5216b7b0d0b95e30f23aa9124d5e0f012
Zachary Allaun <zach.allaun@gmail.com>
2012-10-03 12:30:33 -0400

    Deatomify registers and create a definstruction macro.

--- src/secd/core.clj
+++ src/secd/core.clj
@@ -1,33 +1,27 @@
 (ns secd.core)
 
-(defn atom? [x]
-  (instance? clojure.lang.Atom x))
-
-(defn atom-if-not
-  "Returns x as an Atom, unless it already is."
-  [x]
-  (if (atom? x) x (atom x)))
-
 (defrecord SECDRegisters [f-pointer stack env code dump])
 
 (defn secd-registers
-  [& {:keys [f-pointer stack env code dump] :as registers}]
-  (let [registers (reduce (fn [acc [k v]]
-                            (assoc acc k (atom-if-not v)))
-                          {} registers)
-        defaults {:f-pointer (atom 0)
-                  :stack (atom [])
-                  :env (atom [])
-                  :code (atom [])
-                  :dump (atom [])}]
+  [& {:keys [stack env code dump] :as registers}]
+  (let [defaults (zipmap [:stack :env :code :dump]
+                         (repeat 4 []))]
     (merge defaults registers)))
 
 ;; SECD Basic Instruction Set
+
+(defmulti doinstruct
+  (fn [op registers] op))
+
+(defmacro definstruct
+  [op register-binding & body]
+  `(defmethod doinstruct ~op [op# ~register-binding]
+     ~@body))
 
 ;; Access objects and push values to the stack:
 ;; NIL ::  s e (NIL.c) d      => (nil.s) e c d
 ;; LDC ::  s e (LDC x.c) d    => (x.s) e c d
 ;; LD  ::  s e (LD (i.j).c) d => ((locate (i.j) e).s) e c d
 
-;; (definstruct :nil {:keys [stack] :as registers}
-;;   (assoc registers :stack (cons nil stack)))
+(definstruct :nil {:keys [stack] :as registers}
+  (assoc registers :stack (cons nil stack)))
