3f617a8fa2dcacd258dce3411c4eefa279093dac
Zachary Allaun <zach.allaun@gmail.com>
2012-10-03 16:25:14 -0400

    Implement basic instruction set, minus :rap
    Who needs looping?

--- src/secd/core.clj
+++ src/secd/core.clj
@@ -1,13 +1,13 @@
 (ns secd.core)
 
-(defrecord SECDRegisters [f-pointer stack env code dump])
+;; (defrecord SECDRegisters [f-pointer stack env code dump])
 
 (defn secd-registers
   [& {:keys [stack env code dump] :as registers}]
-  (let [defaults {:stack '()
-                  :env {}
-                  :code '()
-                  :dump '()}]
+  (let [defaults {:stack ()
+                  :env ()
+                  :code ()
+                  :dump ()}]
     (merge defaults registers)))
 
 ;; SECD Basic Instruction Set
@@ -65,3 +65,48 @@
 (defbinary :cons cons)
 (defbinary :add +)
 (defbinary :sub -)
+
+;; If-Then-Else instructions
+
+(definstruct :sel {:keys [stack code dump] :as registers}
+  (let [test (peek stack)
+        [then else & rest] code
+        result (if (not (false? test)) then else)]
+    (assoc registers
+      :stack (pop stack)
+      :code result
+      :dump (cons rest dump))))
+
+(definstruct :join {:keys [code dump] :as registers}
+  (assoc registers
+    :code (peek dump)
+    :dump (pop dump)))
+
+;; Non-recursive function instructions
+
+(definstruct :ldf {:keys [code env stack] :as registers}
+  (assoc registers
+    :stack (cons [(peek code) env] stack)
+    :code (pop code)))
+
+(definstruct :ap {:keys [stack env code dump]}
+  (let [[closure args & rest] stack
+        [function context] closure]
+    (secd-registers :env (cons args context)
+                    :code function
+                    :dump (concat [rest env code] dump))))
+
+(definstruct :rtn {:keys [stack env code dump] :as registers}
+  (let [[s e c & d] dump]
+    (assoc registers
+      :stack (cons (peek stack) s)
+      :env e
+      :code c
+      :dump d)))
+
+;; Recursive function instructions
+
+(definstruct :dum {:keys [env] :as registers}
+  (assoc registers :env (cons nil env)))
+
+;; TODO: recursive apply (:rap)
--- test/secd/core_test.clj
+++ test/secd/core_test.clj
@@ -2,4 +2,88 @@
   (:use midje.sweet
         secd.core))
 
-(fact 1 => 1)
+(fact "about SECD register defaults"
+      (secd-registers) => {:stack () :env () :code () :dump ()}
+      (secd-registers :stack '(:a :b :c)) => {:stack '(:a :b :c)
+                                              :env () :code () :dump ()})
+
+(fact "about :nil instruction"
+      (doinstruct :nil (secd-registers)) => (secd-registers :stack '(nil)))
+
+(fact "about :ldc instruction"
+      (let [registers (secd-registers :code '(:a))]
+        (doinstruct :ldc registers) => (secd-registers :stack '(:a))))
+
+(fact "about :ld instruction"
+      (doinstruct :ld (secd-registers :code '(:key) :env {:key :val}))
+      => (secd-registers :stack '(:val) :env {:key :val})
+
+      (doinstruct :ld (secd-registers :code '(:key)))
+      => (secd-registers :stack '(:secd.core/unbound)))
+
+(fact "about unary built-ins"
+      (doinstruct :atom (secd-registers :stack '(:a)))
+      => (secd-registers :stack '(true))
+
+      (doinstruct :atom (secd-registers :stack '([])))
+      => (secd-registers :stack '(false))
+
+      (doinstruct :car (secd-registers :stack '((1))))
+      => (secd-registers :stack '(1))
+
+      (doinstruct :cdr (secd-registers :stack '((1 2 3))))
+      => (secd-registers :stack '((2 3))))
+
+(fact "about binary built-ins"
+      (doinstruct :cons (secd-registers :stack '(1 (2 3))))
+      => (secd-registers :stack '((1 2 3)))
+
+      (doinstruct :add (secd-registers :stack '(1 1)))
+      => (secd-registers :stack '(2))
+
+      (doinstruct :sub (secd-registers :stack '(1 1)))
+      => (secd-registers :stack '(0)))
+
+(fact "about if-then-else instructions"
+      (let [truthy-sel (secd-registers :stack '(:truthy)
+                                       :code '(:for-true :for-false :rest))
+            falsey-sel (secd-registers :stack '(false)
+                                       :code '(:for-true :for-false :rest))]
+
+        (doinstruct :sel truthy-sel)
+        => (secd-registers :code :for-true :dump '((:rest)))
+
+        (doinstruct :sel falsey-sel)
+        => (secd-registers :code :for-false :dump '((:rest))))
+
+      (doinstruct :join (secd-registers :dump '((:dumped))))
+      => (secd-registers :code '(:dumped)))
+
+(fact "about :ldf instruction"
+      (doinstruct :ldf (secd-registers :code '(:fn-instructions) :env '(:context)))
+      => (secd-registers :stack '([:fn-instructions (:context)])
+                         :env '(:context)))
+
+(fact "about :ap instruction"
+      (let [registers (secd-registers :stack '([:fn-instructions (:context)]
+                                                 :args :rest))]
+        (doinstruct :ap registers)
+        => (secd-registers :env '(:args :context)
+                           :code :fn-instructions
+                           :dump '((:rest) () ()))))
+
+(fact "about :rtn instruction"
+      (let [registers (secd-registers :stack '(:kept :discarded)
+                                      :env '(:discarded)
+                                      :code '(:discarded)
+                                      :dump '((:rest) :env :code))]
+        (doinstruct :rtn registers)
+        => (secd-registers :stack '(:kept :rest)
+                           :env :env
+                           :code :code
+                           :dump nil)))
+
+(fact "about :dum instruction"
+      (doinstruct :dum (secd-registers)) => (secd-registers :env '(nil))
+      (doinstruct :dum (secd-registers :env '(1 2 3)))
+      => (secd-registers :env '(nil 1 2 3)))
