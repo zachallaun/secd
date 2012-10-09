778374f8b6e2d1dbe64a774dab77083941957bd1
Zachary Allaun <zach.allaun@gmail.com>
2012-10-09 13:25:47 -0400

    Add tests for nnth

--- test/secd/util_test.clj
+++ test/secd/util_test.clj
@@ -8,3 +8,11 @@
       (atom? {})      => falsey
       (atom? (ref 0)) => falsey
       (atom? (atom 0) => truthy))
+
+(fact "about nnth"
+      (nnth '(()) 0 0)     => (throws java.lang.IndexOutOfBoundsException)
+      (nnth [[]] 0 0)      => (throws java.lang.IndexOutOfBoundsException)
+      (nnth '((1)) 0 0)    => 1
+      (nnth [[1]] 0 0)     => 1
+      (nnth '(() (1)) 1 0) => 1
+      (nnth [[] [1]] 1 0)  => 1)
