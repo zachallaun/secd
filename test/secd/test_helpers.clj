20bddf894da93fdc9201475f28273296f98432e3
Zachary Allaun <zach.allaun@gmail.com>
2012-10-09 12:31:10 -0400

    Create map-similar-to for testing maps with atoms.

--- test/secd/test_helpers.clj
+++ test/secd/test_helpers.clj
@@ -1,11 +1,14 @@
-(ns secd.test-helpers)
+(ns secd.test-helpers
+  (:use midje.sweet
+        [secd.util :only [atom?]]))
 
-(defn structure-checker [selector]
+(defn structure-checker
   "Returns a function used to generate higher-order functions that can act as
   composable predicates that verify attributes of some data structure.
 
   ex. (def stack-is (structure-checker :stack))
       ((stack-is []) {:stack []})"
+  [selector]
   (fn [pred]
     (fn [structure]
       (if (fn? pred)
@@ -17,3 +20,29 @@
 (def env-is (structure-checker :env))
 (def code-is (structure-checker :code))
 (def dump-is (structure-checker :dump))
+
+(defn map-similar-to
+  "Returns a function that assesses similarity. Assumes coll and coll2 to be
+  an associative structure with values that may be atoms. Two atoms are similar
+  if their dereferenced values are the same."
+  [coll]
+  {:pre [(map? coll)]}
+  (fn [coll2]
+    {:pre [(map? coll2)]}
+    (and (every? (set (keys coll)) (keys coll2))
+         (every? (fn [[k a]]
+                   (let [v (get coll2 k)]
+                     (if (and (atom? a) (atom? v))
+                       (= @a @v)
+                       (= a v))))
+                 coll))))
+
+(fact "about map-similar-to"
+      (map-similar-to []) => (throws AssertionError)
+      ((map-similar-to {}) []) => (throws AssertionError)
+
+      ((map-similar-to {})             {}) => truthy
+      ((map-similar-to {:k :v})        {:k :v}) => truthy
+      ((map-similar-to {:k (atom :v)}) {:k (atom :v)}) => truthy
+      ((map-similar-to {:k :v})        {:k (atom :v)}) => falsey
+      ((map-similar-to {:k :v})        {:k :v :k2 :v2}) => falsey)
